5f1851ba53abd1b0284cb5be0ddbb4502619bfdd
9um4 <51898564+9um4@users.noreply.github.com>
2024-01-10 01:46:13 +0900

    init: Initialize Project

---
+++ src/main/java/io/github/vitelotte/nickname/Nickname.java
@@ -0,0 +1,17 @@
+package io.github.vitelotte.nickname;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Nickname extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
